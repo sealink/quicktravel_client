3d5b510d5f6bf742fd14ee2a99dd929d1b8ff935
Adam Davies <adzdavies@gmail.com>
2015-03-04 12:23:36 +1030

    Don't try to use facets underscore -- conflicts with autoload

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -1,2 +1,12 @@
 require 'facets/kernel/blank'
-require 'facets/string/underscore'
+
+# Can not use. It inteferes with active support underscore
+# which is used by auto load! Such fun!
+#
+# See
+#  * core/facets/string/underscore.rb
+#  * core/facets/string/snakecase.rb
+#
+# Note, this method no longer converts `::` to `/`, in that case
+# use the {#pathize} method instead.
+#require 'facets/string/underscore'
