6f89bf4906e5cde7837af7b7cfe0398a3a18d9b4
Michael Noack <michael@noack.com.au>
2015-11-18 12:03:11 +1030

    Make product types use new API controller
     * Switch to 8080 port for all requests (tenant was odd one out)

--- spec/tenant_spec.rb
+++ spec/tenant_spec.rb
@@ -3,8 +3,8 @@ require 'quick_travel/adapter'
 require 'quick_travel/product_type'
 
 describe QuickTravel::Adapter do
-  let(:tenant1) { 'http://sealink.dev:3000' }
-  let(:tenant2) { 'http://sealinkqld.dev:3000' }
+  let(:tenant1) { 'http://sealink.dev:8080' }
+  let(:tenant2) { 'http://sealinkqld.dev:8080' }
 
   let(:qt_keys) { ENV['QT_KEYS'].split(',') }
 
@@ -14,14 +14,14 @@ describe QuickTravel::Adapter do
       QuickTravel.config.access_key = qt_keys[0]
 
       expect(QuickTravel::Adapter.base_uri).to eq tenant1
-      QuickTravel::ProductType.find_all!("/product_types.json")
+      QuickTravel::ProductType.find_all!("/api/product_types.json")
       expect(QuickTravel::ProductType.base_uri).to eq tenant1
 
       QuickTravel::Adapter.base_uri tenant2
       QuickTravel.config.access_key = qt_keys[1]
 
       expect(QuickTravel::Adapter.base_uri).to eq tenant2
-      QuickTravel::ProductType.find_all!("/product_types.json")
+      QuickTravel::ProductType.find_all!("/api/product_types.json")
       expect(QuickTravel::ProductType.base_uri).to eq tenant2
     end
   end
