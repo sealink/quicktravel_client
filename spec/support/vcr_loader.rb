f644df82d111ff807e3b94753b56b06ff1f16857
Michael Noack <michael@noack.com.au>
2016-05-10 08:05:20 +0930

    Extract vcr into loader and fix due to webmock/vcr upgrades

---
+++ spec/support/vcr_loader.rb
@@ -0,0 +1,12 @@
+require 'vcr'
+require 'webmock/rspec'
+
+qt_keys = ENV['QT_KEYS'].split(',')
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/support/cassettes'
+  c.default_cassette_options = { match_requests_on: [:method, :uri, :body] }
+  c.filter_sensitive_data('<QT_KEY>')   { qt_keys[0] }
+  c.filter_sensitive_data('<QT_KEY_2>') { qt_keys[1] }
+  c.hook_into :webmock
+end
