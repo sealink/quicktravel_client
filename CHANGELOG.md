be5d1f8be486db38d6e5142c155428cc99033b28
Michael Noack <michael@noack.com.au>
2017-02-08 10:41:13 +1030

    Remove unused property constants

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 This changelog adheres to [Keep a CHANGELOG](http://keepachangelog.com/).
 
+## [2.9.0]
+### Removed
+- Remove unused constants
+
 ## [2.8.0]
 ### Changed
 - @booking.accommodation_reserve now expects to be passed the adjusted last_travel_date
--- lib/quick_travel/constants.rb
+++ lib/quick_travel/constants.rb
@@ -3,12 +3,7 @@
 module QuickTravel
   LOGIN_URL  = '/parties/login' # CMS_SSL_URL + '/parties/login'
 
-  DEFAULT_LOCATION_ID_FOR_ACCOMM_SEARCH = 3 # Kingscote
-  DEFAULT_REGION_ID_FOR_ACCOMM_SEARCH = 1   # Kangaroo Island
-  DEFAULT_PROPERTY_TYPE_ID_FOR_ACCOMM_SEARCH = 1
-
   # System wide
-  PROPERTY_CACHE_TIMEOUT = 1440 # minutes
   QT_DATE_FORMAT = '%d-%m-%Y'
 
   DEFAULT_REFERRAL_CODE_ID = 33
