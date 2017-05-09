16b12bb1a5845be198f7eb99ad1828b6c39386c0
John Mortlock <john.mortlock@gmail.com>
2017-05-09 16:14:31 +0930

    rename extra_picks_with_price to selected_extra_picks_with_price

--- lib/quick_travel/product_configuration.rb
+++ lib/quick_travel/product_configuration.rb
@@ -102,24 +102,24 @@ module QuickTravel
       end
     end
 
-    def extra_picks_with_price
+    def selected_extra_picks_with_price
       selected_extra_pick_configurations.select(&:priced?)
     end
 
     def selected_extra_picks_price
-      total_money(extra_picks_with_price.map(&:price))
+      total_money(selected_extra_picks_with_price.map(&:price))
     end
 
     def selected_extra_picks_price_without_rules
-      total_money(extra_picks_with_price.map(&:price_without_rules))
+      total_money(selected_extra_picks_with_price.map(&:price_without_rules))
     end
 
     def selected_extra_picks_price_for_rack_rate
-      total_money(extra_picks_with_price.map(&:price_for_rack_rate))
+      total_money(selected_extra_picks_with_price.map(&:price_for_rack_rate))
     end
 
     def selected_extra_picks_applied_rules
-      extra_picks_with_price.flat_map(&:applied_rules)
+      selected_extra_picks_with_price.flat_map(&:applied_rules)
     end
 
     def total_money(array)
