e63d362e45258323dc8227a137c8bd05b4af5d77
Stefan Cooper <stefan.cooper@sealink.com.au>
2018-11-20 09:49:55 +1030

    Client template specs

---
+++ spec/client_templates_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'quick_travel/client_templates'
+
+describe QuickTravel::ClientTemplates do
+  context '#find' do
+    let(:templates) do
+      VCR.use_cassette('client_templates') do
+        QuickTravel::ClientTemplates.find(2)
+      end
+    end
+    it 'should find the templates correctly' do
+
+      expect(templates.vehicle_templates).to eq([
+        {
+          'id' => 1,
+          'length' => 5.0,
+          'height' => 1.5,
+          'width' => 1.8,
+          'weight' => 1000.0,
+          'details' => 'Holden Commodore',
+          'registration' => 'ABC123',
+          'cargo' => 'None',
+          'vehicle_type_id' => 1,
+          'party_id' => 9
+        }
+      ])
+      expect(templates.passenger_templates).to eq([
+        {
+          'id' => 1,
+          'age' => 30,
+          'title' => 'Mr',
+          'first_name' => 'Homer',
+          'last_name' => 'Simpson',
+          'gender' => 'Male',
+          'passenger_type_id' => 1,
+          'party_id' => 9
+        }
+      ])
+    end
+  end
+end
